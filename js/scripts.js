465433ee00a4da429d7032971e6e80e809acd6eb
Bryson69 <brysonmundia@gmail.com>
2020-05-10 19:36:52 +0300

    Added jquery toggle effect on what we do

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// what we do 
+$('.first').click(function () {
+    $(this).children('.drop').slideToggle(1300);
+    $(this).children('.show').slidetoggle(1400);
+  });
+// portfolio
+
+// feedback
