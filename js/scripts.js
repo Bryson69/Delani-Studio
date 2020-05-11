bdc9a3c2f0b518dddb241af183c4f60b992d927b
Bryson69 <brysonmundia@gmail.com>
2020-05-11 23:26:10 +0300

    Added js properties

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,22 @@
 // what we do 
 $('.first').click(function () {
-    $(this).children('.drop').slideToggle(1300);
-    $(this).children('.show').slidetoggle(1400);
-  });
+  $(this).children('.drop').slideToggle(1300);
+  $(this).children('.show').slidetoggle(1400);
+});   
 // portfolio
+$(".card").hover(function () {
+  $(this).children(".card-1").slideToggle(1000, "linear");
+});
 
-// feedback
+// 
+$("button").click(function (Bryson) {
+  var client = document.getElementById('nameDetail').value;
+  alert(' Dear ' + ' customer ' + ' we have received your message successfully. ' + ' reach out to us anytime');
+  Bryson.preventDefault();
+});
+//   button
+$("button").on('click', function () {
+  $('form').each(function () {
+    this.reset();
+  });
+});
